29061e04f9219b57e0b4a9d9d38ca5945f10b8fa
Saddasuchithreddy <saddasuchithreddy@gmail.com>
2025-03-07 11:35:56 +0530

    with 2 variables

--- backend/django_app/urls.py
+++ backend/django_app/urls.py
@@ -6,16 +6,18 @@ from django.http import JsonResponse
 
 def hello_name(request):
     """
-    A simple view that returns 'Hello, {name}' in JSON format.
-    Uses a query parameter named 'name'.
+    A simple view that returns 'Hello, {name}, age : {age} ' in JSON format.
+    Uses query parameters named 'name' and age
     """
     # Get 'name' from the query string, default to 'World' if missing
+    # Get age from the query string, default to Unknown
     name = request.GET.get("name", "World")
-    return JsonResponse({"message": f"Hello, {name}!"})
+    age = request.GET.get("age","Unknown")
+    return JsonResponse({"message": f"Hello, {name}!" , "age": age })
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello/', hello_name), 
-    # Example usage: /hello/?name=Bob
-    # returns {"message": "Hello, Bob!"}
+    # Example usage: /hello/?name=BOB&age=20
+    # returns {"message":"Hello, BOB!","age":"20"}
 ]
